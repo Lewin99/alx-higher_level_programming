21e16ce5bce329710fab49094b75a6cc55bd90e9
Lewin99 <88319929+Lewin99@users.noreply.github.com>
2022-09-02 22:36:08 +0200

    done

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    b=str(number)
-    print("{:s}".format(b[len(b)-1]))
+    if number >= 0:
+        ld = number % 10
+    else:
+        ld = number % -10
+        ld *= -1
+
+    print("{:d}".format(ld), end='')
+    return (ld)
