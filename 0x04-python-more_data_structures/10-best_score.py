65db4effa02e2e60cccaa345d4f6c9ba9658fa26
Lewin99 <88319929+Lewin99@users.noreply.github.com>
2022-09-29 15:54:01 +0400

    done

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary.keys() is not None:
-        ma=max(a_dictionary)
-        return ma
+    if not a_dictionary:
+        return (None)
+
+    return (max(a_dictionary, key=a_dictionary.get))
