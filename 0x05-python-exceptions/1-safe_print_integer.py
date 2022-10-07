9d7c58b1ceaf3984551933e5f613b1827ed127b6
Lewin99 <88319929+Lewin99@users.noreply.github.com>
2022-10-07 15:38:53 +0400

    done

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+def safe_print_integer(value):
+    try:
+        print("{:d}".format(value))
+        return 'true'
+    except ValueError:
+        pass
+        return 'false'
