d2248b74135d0b488b18ccdfee7c4f5bc1e31634
Lewin99 <88319929+Lewin99@users.noreply.github.com>
2022-11-01 17:45:22 +0400

    DONE

--- 0x0D-SQL_introduction/8-count_89.sql
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -1,2 +1,2 @@
 -- Write a script that displays the number of records with id = 89 in the table first_table of the database 
-SELECT count( * ) FROM `first_class` WHERE id=89;
+SELECT COUNT( * ) FROM `first_class` WHERE `id`=89;
