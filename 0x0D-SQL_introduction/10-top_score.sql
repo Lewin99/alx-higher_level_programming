2ae91f27c2119357992d8e24bfb6ec7423e99980
Lewin99 <88319929+Lewin99@users.noreply.github.com>
2022-11-01 18:07:24 +0400

    done

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,2 +1,5 @@
--- ordered
-SELECT * FROM `second_table` ORDER BY `score` DESC
+-- Lists all records of the table second_table.
+-- Records are ordered by descending score.
+SELECT `score`, `name`
+FROM `second_table`
+ORDER BY `score` DESC;
