e988d1587b2b373ea3e83c6f8b469478ad5b433f
Lewin99 <88319929+Lewin99@users.noreply.github.com>
2022-11-01 17:37:28 +0400

    done

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,2 +1,2 @@
 --  a script that displays the number of records with id = 89 in the table first_table of the database hbtn_0c
-INSERT INTO `first_table`(id INT) VALUES(89);
+INSERT INTO `first_table`(`id INT`,`name`) VALUES(89,"Best School");
