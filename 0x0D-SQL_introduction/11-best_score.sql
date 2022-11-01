adf468a877d534ca7d3cb9c91f7305b24563edcf
Lewin99 <88319929+Lewin99@users.noreply.github.com>
2022-11-01 18:11:45 +0400

    done

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,1 +1,5 @@
-iqq
+-- Lists all records of the table second_table.
+-- Records are ordered by descending score.
+SELECT `score`, `name`
+FROM `second_table` WHERE `score1 >= 10
+ORDER BY `score` DESC;
