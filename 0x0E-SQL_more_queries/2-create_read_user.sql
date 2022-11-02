9c845ab2f24ec39f0e5aae58dc63844c8dc98e22
Lewin99 <88319929+Lewin99@users.noreply.github.com>
2022-11-02 23:33:10 +0400

    done

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,4 +1,7 @@
--- user,database and set select priileges
-CREATE USER IF NOT EXISTS user_0d_1@localhost IDENTIFIED BY 'user_0d_1_pwd';
-CREATE DATABASE IF NOT EXISTS 'hbtn_0d_2';
-GRANT PRIVILEGE SELECT ON hbtn_0d_2.* TO user_0d_1 WITH GRANT OPTION;
+-- creates the database hbtn_0d_2 and the user user_0d_2
+-- creates a database
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+-- creates a user
+CREATE USER IF NOT EXISTS user_0d_2@localhost IDENTIFIED BY 'user_0d_2_pwd';
+-- grants SELECT privileges to a user
+GRANT SELECT ON hbtn_0d_2.* TO user_0d_2@localhost;
