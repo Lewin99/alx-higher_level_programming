3f81fbd3c75c8c5474a7c477095050f75ce5e954
Lewin99 <88319929+Lewin99@users.noreply.github.com>
2022-11-03 00:02:24 +0400

    done

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,3 +1,8 @@
--- database and table with multiple constraints
-CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
-ICREATE TABLE IF NOT EXISTS `states` (`id` INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,`name` VARCHAR(256) NOT NULL);
+-- creates the database hbtn_0d_usa and the table states (in the database hbtn_0d_usa) on your MySQL server
+-- creates a database
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+-- use a database
+USE hbtn_0d_usa;
+-- creates a table
+CREATE TABLE IF NOT EXISTS states (id INT UNIQUE NOT NULL AUTO_INCREMENT, name VARCHAR(256) NOT NULL, PRIMARY KEY(id));
+
