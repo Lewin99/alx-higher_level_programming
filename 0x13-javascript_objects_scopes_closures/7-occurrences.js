b3e84c501dfbf9bc0dc18ea58149f78a1b4c16e2
Lewin99 <88319929+Lewin99@users.noreply.github.com>
2022-12-03 17:41:12 +0400

    done

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,2 +1,10 @@
-do
-
+#!/usr/bin/node
+exports.nbOccurences = function (list, searchElement) {
+  let nOccurrences = 0;
+  for (let i = 0; i < list.length; i++) {
+    if (searchElement === list[i]) {
+      nOccurrences++;
+    }
+  }
+  return nOccurrences;
+};
